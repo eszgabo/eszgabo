5c6a703cccea0ad6fbf6767efc22956005fae760
user212244 <szonyig1@gmail.com>
2022-03-02 11:22:00 +0100

    First commit

---
+++ Adnijo/Controllers/Core Tabs/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  Adnijo
+//
+//  Created by user212244 on 3/2/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
